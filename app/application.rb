f7106a4e3f75045aba3b4dc7b01c36aa449dc53e
Susan Tan <ssusantan9@gmail.com>
2019-04-21 01:00:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,9 @@ class Application
   
   def call(evn)
     resp = Rack::Response.new
-    
-    resp.write Time.now
+    if Time.now.hour < 12 
+    resp.write "Good Morning!"
+  else 
+    "Good Afternoon!"
   end
 end 
